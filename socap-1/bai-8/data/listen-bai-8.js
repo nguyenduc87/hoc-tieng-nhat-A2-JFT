ec83a7fefc89253e772d2541c6be8f38df0d2524
Nguyen Tri Duc <nguyenducdaito@gmail.com>
2026-03-16 23:30:17 +0700

    bo sung nghe bai 8

--- socap-1/bai-8/data/listen-bai-8.js
+++ socap-1/bai-8/data/listen-bai-8.js
@@ -1,200 +1,438 @@
 export const listeningData = [
   {
     title: "Audio 1 : kiku1",
-    audio: "audio/Y_[07-01]_kiku1.mp3",
+    audio: "audio/Y_[08-01]_kiku1.mp3",
     script: [
-      "A：すみません。明日は何時にどこですか。",
-      "B：明日は、朝8時に会社の門の前に集合【しゅうごう】ね。",
-      "A：わかりました。8時に門の前ですね。",
-      "B：じゃあ、また明日。"
+      "A：新しいアウトレットモール、もう行きましたか。",
+      "B：いいえ、まだです。",
+      "A：今度、みんなで行きます。いっしょに行きませんか。",
+      "B：ありがとうございます。ぜひ。"
     ],
     image: [
-      "images/listen/07-01.png",
+      "images/listen/08-01.png",
     ],
     translation: [
-      "A: Xin lỗi. Ngày mai mấy giờ và ở đâu?",
-      "B: Ngày mai chúng ta tập trung trước cổng công ty lúc 8 giờ sáng nhé.",
-      "A: Tôi hiểu rồi. 8 giờ trước cổng nhé.",
-      "B: Vậy hẹn gặp lại ngày mai."
+      "A: Trung tâm mua sắm outlet mới, bạn đã đi chưa?",
+      "B: Chưa, tôi vẫn chưa đi.",
+      "A: Lần tới mọi người sẽ đi cùng nhau. Bạn đi cùng không?",
+      "B: Cảm ơn. Nhất định rồi."
     ]
   },
   {
     title: "Audio 2 : kiku2",
-    audio: "audio/Y_[07-02]_kiku2.mp3",
+    audio: "audio/Y_[08-02]_kiku2.mp3",
     script: [
-      "A：では、金曜日の10時に来てください。",
-      "B：金曜日の10時ですね。えーと、場所はどこですか。",
-      "A：受付に来てください。そのあと、部屋に案内します。",
-      "B：わかりました。"
+      "A：「パームキッチン」っていうアジアレストラン、行ったことありますか。",
+      "B：いいえ、ありません。",
+      "A：すごくおいしいですよ。今度、いっしょに食べに行きませんか。",
+      "B：いいですね。ぜひ、行きましょう。"
     ],
     image: [
-      "images/listen/07-01.png",
+      "images/listen/08-02.png",
     ],
     translation: [
-      "A: Vậy hãy đến vào 10 giờ thứ Sáu.",
-      "B: 10 giờ thứ Sáu đúng không. Ờ… địa điểm ở đâu vậy?",
-      "A: Hãy đến quầy lễ tân. Sau đó tôi sẽ dẫn bạn vào phòng.",
-      "B: Tôi hiểu rồi."
+      "A: Nhà hàng châu Á tên là 'Palm Kitchen', bạn đã từng đến chưa?",
+      "B: Chưa, tôi chưa từng đến.",
+      "A: Rất ngon đó. Lần tới chúng ta cùng đi ăn nhé?",
+      "B: Nghe hay đấy. Nhất định đi thôi."
     ]
   },
   {
     title: "Audio 3 : kiku3",
-    audio: "audio/Y_[07-03]_kiku3.mp3",
+    audio: "audio/Y_[08-03]_kiku3.mp3",
     script: [
-      "A：明日の待ち合わせは、何時にどこにしますか。",
-      "B：じゃあ、6時半に、駅の改札【かいさつ】でいい？",
-      "A：はい。6時半に駅ですね。",
-      "B：じゃあ、また。"
+      "A：プロレスを見たことがありますか。",
+      "B：いいえ、見たことないです。",
+      "A：今度の日曜日、市民体育館で試合がありますよ。いっしょに見に行きませんか。",
+      "B：へー、いいですね。行きましょう。"
     ],
     image: [
-      "images/listen/07-01.png",
+      "images/listen/08-01.png",
     ],
     translation: [
-      "A: Cuộc hẹn ngày mai thì mấy giờ và ở đâu?",
-      "B: Vậy 6 giờ rưỡi ở cổng soát vé nhà ga được không?",
-      "A: Vâng. 6 giờ rưỡi ở nhà ga nhé.",
-      "B: Vậy nhé, hẹn gặp lại."
+      "A: Bạn đã từng xem đấu vật chuyên nghiệp chưa?",
+      "B: Chưa, tôi chưa từng xem.",
+      "A: Chủ nhật tới có trận đấu ở nhà thi đấu thành phố đó. Chúng ta cùng đi xem nhé?",
+      "B: À vậy à, hay đấy. Đi thôi."
     ]
   },
   {
     title: "Audio 4 : kiku4",
-    audio: "audio/Y_[07-04]_kiku4.mp3",
+    audio: "audio/Y_[08-04]_kiku4.mp3",
     script: [
-      "A：あのう、10日のイベント、何時からですか。",
-      "B：17時に始【はじ】まりますが、スタッフは30分前にホールに来てください。",
-      "A：えーと、じゃあ4時半ですね。わかりました。",
-      "B：よろしくお願いします。"
+      "A：ポートタワー、もう登りましたか。",
+      "B：いいえ……。",
+      "A：じゃあ、今度、行きませんか。景色がきれいですよ。",
+      "B：すみません。私、高いところはちょっと……。"
     ],
     image: [
-      "images/listen/07-01.png",
+      "images/listen/08-01.png",
     ],
     translation: [
-      "A: Cho tôi hỏi, sự kiện ngày 10 bắt đầu lúc mấy giờ?",
-      "B: Bắt đầu lúc 17 giờ, nhưng nhân viên hãy đến hội trường trước 30 phút.",
-      "A: À, vậy là 4 giờ rưỡi nhỉ. Tôi hiểu rồi.",
-      "B: Nhờ bạn giúp nhé."
+      "A: Bạn đã lên tháp Port Tower chưa?",
+      "B: Chưa...",
+      "A: Vậy lần tới đi nhé? Cảnh rất đẹp đó.",
+      "B: Xin lỗi. Tôi hơi sợ những nơi cao..."
     ]
   },
   {
-    title: "Audio 5 : kaiwa",
-    audio: "audio/Y_[07-05]_kaiwa.mp3",
+    title: "Audio 5 : kiku5",
+    audio: "audio/Y_[08-05]_kiku5.mp3",
     script: [
-      "はじめに、スクリプトを見ないで会話を聞きましょう。",
-      "→Trước tiên, hãy nghe đoạn hội thoại mà không nhìn lời thoại.",
-      "３人は、何時に、どこで待ち合わせすることにしましたか。",
-      "→3 người họ đã quyết định sẽ gặp nhau lúc mấy giờ và ở đâu?",
-      "何時？：",
-      "どこ？：",
-      "　",
-      "ベト：明日、何時にしますか。",
-      "佐藤【さとう】：6時はどうですか。",
-      "李【イ】：6時はちょっと早いです。6時半でもいいですか。",
-      "ベト：6時半？ いいですよ。",
-      "佐藤【さとう】：じゃあ、そうしましょう。待ち合わせして、いっしょに行きますか。",
-      "李【イ】：そうしましょう。どこにしますか。",
-      "佐藤【さとう】：じゃあ、駅前の「平和【へいわ】の鳥【とり】」の像【ぞう】の前にしましょう。",
-      "ベト：え？ それ、何ですか。",
-      "李【イ】：私もわかりません。駅の改札【かいさつ】でもいいですか。",
-      "佐藤【さとう】：じゃあ、そうしましょう。6時半に駅の改札集合【かいさつしゅうごう】で。",
-      "ベト：はい。",
-      "李【イ】：わかりました。"
+      "A：もう、この町のいろいろなところに行きましたか。",
+      "B：えーと、博物館とお城には行きました。",
+      "A：動物園は？",
+      "B：まだです。行きたいです。",
+      "A：じゃあ、今度いっしょに行きましょう。"
     ],
     image: [
-      "images/listen/07-05.png",
+      "images/listen/08-02.png",
     ],
     translation: [
-      "Beto: Ngày mai mấy giờ thì được?",
-      "Sato: 6 giờ được không?",
-      "Lee: 6 giờ hơi sớm. 6 giờ rưỡi được không?",
-      "Beto: 6 giờ rưỡi à? Được thôi.",
-      "Sato: Vậy quyết định vậy nhé. Hẹn gặp rồi cùng đi nhé?",
-      "Lee: Vậy nhé. Gặp ở đâu?",
-      "Sato: Vậy gặp trước tượng 'Chim hòa bình' trước nhà ga nhé.",
-      "Beto: Hả? Cái đó là gì vậy?",
-      "Lee: Tôi cũng không biết. Gặp ở cổng soát vé nhà ga được không?",
-      "Sato: Vậy cũng được. 6 giờ rưỡi tập trung ở cổng soát vé nhà ga nhé.",
-      "Beto: Vâng.",
-      "Lee: Tôi hiểu rồi."
+      "A: Bạn đã đi nhiều nơi khác nhau trong thành phố này chưa?",
+      "B: Ừm, tôi đã đi bảo tàng và lâu đài rồi.",
+      "A: Còn sở thú thì sao?",
+      "B: Chưa. Tôi muốn đi.",
+      "A: Vậy lần tới chúng ta cùng đi nhé."
     ]
   },
   {
-    title: "Audio 6 : katachi",
-    audio: "audio/Y_[07-06]_katachi.mp3",
+    title: "Audio 6 : kiku6",
+    audio: "audio/Y_[08-06]_kiku6.mp3",
     script: [
-      "A：明日、何時にしますか。",
-      "B：6時は________？",
-      "A：ちょっと早いです。6時半________？",
-      "A：どこにしますか。",
-      "B：駅前の「平和【へいわ】の鳥【とり】」の像【ぞう】の前にしましょう。",
-      "A：わかりません。駅の改札【かいさつ】________？"
+      "A：野球、したことある？",
+      "B：え、ないです。",
+      "A：今度の日曜、いっしょにやらない？ メンバーが足りなくて……。",
+      "B：えー、無理です。野球、ぜんぜんわかりません。",
+      "A：だいじょうぶ、だいじょうぶ。"
+    ],
+    image: [
+      "images/listen/08-01.png",
+    ],
+    translation: [
+      "A: Bạn đã từng chơi bóng chày chưa?",
+      "B: À, chưa.",
+      "A: Chủ nhật tới chơi cùng nhé? Đội đang thiếu người...",
+      "B: Ờ... tôi không được đâu. Tôi hoàn toàn không biết gì về bóng chày.",
+      "A: Không sao đâu, không sao đâu."
+    ]
+  },
+  {
+    title: "Audio 7 : katachi1",
+    audio: "audio/Y_[08-07]_katachi1.mp3",
+    script: [
+      "A：新しいアウトレットモール、________行きましたか。",
+      "B：いいえ、________です。",
+      "---------------------",
+      "A：ポートタワー、________登りましたか。",
+      "B：いいえ……。",
+      "---------------------",
+      "A：「パームキッチン」っていうアジアレストラン、行った________か。",
+      "B：いいえ、________。",
+      "---------------------",
+      "A：プロレスを見た________か。",
+      "B：いいえ、見た________です。",
+      "---------------------",
+      "A：野球、した________？",
+      "B：え、________です。"
     ],
 
     translation: [
-      "1. Ngày mai mấy giờ thì được?",
-      "2. 6 giờ ________? → どうですか",
-      "3. Hơi sớm. 6 giờ rưỡi ________? → でもいいですか",
-      "4. Gặp ở đâu?",
-      "5. Hãy gặp trước tượng 'Chim hòa bình' trước nhà ga.",
-      "6. Tôi không biết. Ở cổng soát vé nhà ga ________? → でもいいですか"
+      "1. Trung tâm outlet mới, ________ bạn đã đi chưa? → もう",
+      "2. Không, ________ → まだ",
+      "---------------------",
+      "3. Tháp Port Tower, ________ bạn đã lên chưa? → もう",
+      "4. Chưa...",
+      "---------------------",
+      "5. Nhà hàng châu Á tên 'Palm Kitchen', bạn đã từng đi ________ chưa? → ことがあります",
+      "6. Không, ________ → ありません",
+      "---------------------",
+      "7. Bạn đã từng xem đấu vật ________ chưa? → ことがあります",
+      "8. Không, tôi ________ → ことないです",
+      "---------------------",
+      "9. Bạn đã từng chơi bóng chày ________? → ことある",
+      "10. À, ________ → ない"
     ]
   },
   {
-    title: "Audio 7 : hanasu1",
-    audio: "audio/Y_[07-07]_hanasu1.mp3",
+    title: "Audio 8 : katachi2",
+    audio: "audio/Y_[08-08]_katachi2.mp3",
     script: [
-      "A：明日の待ち合わせ、何時にどこにしますか。",
-      "B：6時に「平和の鳥」の像の前はどうですか。",
-      "A：6時に「平和の鳥」の像ですね？",
-      "B：はい。",
-      "A：わかりました。"
+      "A：市民体育館で試合【しあい】がありますよ。<br>いっしょに________行き________？",
+      "-----------------------------",
+      "B：いいですね。行きましょう。",
+      "A：すごくおいしいですよ。<br>今度、いっしょに________行き________？",
+      "B：いいですね。ぜひ、行きましょう。"
     ],
+
     translation: [
-      "A: Cuộc hẹn ngày mai thì mấy giờ và ở đâu?",
-      "B: 6 giờ trước tượng 'Chim hòa bình' được không?",
-      "A: 6 giờ trước tượng 'Chim hòa bình' đúng không?",
-      "B: Vâng.",
-      "A: Tôi hiểu rồi."
-    ]
-  },
-  {
-    title: "Audio 8 : hanasu2",
-    audio: "audio/Y_[07-08]_hanasu2.mp3",
-    script: [
-      "A：明日の待ち合わせ、何時にどこにしますか。",
-      "B：6時に「平和の鳥」の像の前はどうですか。",
-      "A：「平和の鳥」の像はちょっとわかりません。",
-      "A：駅の改札でもいいですか。",
-      "B：いいですよ。",
-      "B：じゃあ、6時に駅の改札で。"
-    ],
-    translation: [
-      "A: Cuộc hẹn ngày mai thì mấy giờ và ở đâu?",
-      "B: 6 giờ trước tượng 'Chim hòa bình' được không?",
-      "A: Tôi không rõ tượng 'Chim hòa bình'.",
-      "A: Gặp ở cổng soát vé nhà ga được không?",
-      "B: Được thôi.",
-      "B: Vậy 6 giờ ở cổng soát vé nhà ga nhé."
+      "1. Có trận đấu ở nhà thi đấu thành phố đó. <br>Cùng ________ đi xem nhé? → 見に / ませんか",
+      "-----------------------------",
+      "2. Nghe hay đấy. Đi thôi.",
+      "3. Rất ngon đó. <br>Lần tới cùng ________ đi ăn nhé? → 食べに / 行きませんか",
+      "4. Hay đấy. Nhất định đi thôi."
     ]
   },
   {
     title: "Audio 9 : hanasu3",
-    audio: "audio/Y_[07-09]_hanasu3.mp3",
+    audio: "audio/Y_[08-09]_hanasu1.mp3",
     script: [
-      "A：明日の待ち合わせ、何時にどこにしますか。",
-      "B：6時に「平和の鳥」の像の前はどうですか。",
-      "A：6時はちょっと早いです。",
-      "A：6時半でもいいですか。",
-      "B：いいですよ。",
-      "B：じゃあ、6時半に「平和の鳥」の像の前で。"
+      "A：新しいアウトレットモール、もう行きましたか。",
+      "B：いいえ、まだです。",
+      "A：じゃあ、今度、いっしょに行きませんか。",
+      "B：ありがとうございます。ぜひ。"
     ],
+
     translation: [
-      "A: Cuộc hẹn ngày mai thì mấy giờ và ở đâu?",
-      "B: 6 giờ trước tượng 'Chim hòa bình' được không?",
-      "A: 6 giờ hơi sớm.",
-      "A: 6 giờ rưỡi được không?",
-      "B: Được thôi.",
-      "B: Vậy 6 giờ rưỡi trước tượng 'Chim hòa bình' nhé."
+      "1. Trung tâm outlet mới, bạn đã đi chưa?",
+      "2. Chưa, tôi vẫn chưa đi.",
+      "3. Vậy lần tới chúng ta cùng đi nhé?",
+      "4. Cảm ơn. Nhất định rồi."
     ]
   },
-  
+  {
+    title: "Audio 10 : hanasu2-1",
+    audio: "audio/Y_[08-10]_hanasu2-1.mp3",
+    script: [
+      "A：プロレスを見たことがありますか。",
+      "B：いいえ、ないです。",
+      "A：今度の日曜日、市民【しみん】体育館【たいいくかん】で試合【しあい】がありますよ。いっしょに見に行きませんか。",
+      "B：へー、いいですね。行きましょう。"
+    ],
+
+    translation: [
+      "1. Bạn đã từng xem đấu vật chuyên nghiệp chưa?",
+      "2. Chưa, tôi chưa từng.",
+      "3. Chủ nhật tới có trận đấu ở nhà thi đấu thành phố đó. Cùng đi xem nhé?",
+      "4. À vậy à, hay đấy. Đi thôi."
+    ]
+  },
+  {
+    title: "Audio 11 : hanasu2-2",
+    audio: "audio/Y_[08-11]_hanasu2-2.mp3",
+    script: [
+      "A：プロレスを見たことがありますか。",
+      "B：いいえ、ありません。",
+      "A：今度の日曜日、市民【しみん】体育館【たいいくかん】で試合【しあい】がありますよ。いっしょに見に行きませんか。",
+      "B：えー、すみません。プロレスはちょっと……。"
+    ],
+
+    translation: [
+      "1. Bạn đã từng xem đấu vật chuyên nghiệp chưa?",
+      "2. Không, tôi chưa từng.",
+      "3. Chủ nhật tới có trận đấu ở nhà thi đấu thành phố đó. Cùng đi xem nhé?",
+      "4. À... xin lỗi. Tôi hơi không thích đấu vật..."
+    ]
+  },
+  {
+    title: "Audio 12 : kaiwa",
+    audio: "audio/Y_[08-12]_kaiwa.mp3",
+    script: [
+      "（歩きながら）",
+      "アニタ：このあと、どうしますか。",
+      "ヒルニ：お腹がすきましたね。何か食べませんか。",
+      "アニタ：じゃあ、フードコートに行きましょう。",
+      "ソック：あ、食事の前に、お金をおろしたいんですが……。",
+      "アニタ：あそこにATMがありますよ。",
+      "　",
+      "（フードコートで）",
+      "ヒルニ：このあと、ジョギングシューズを買いたいんですが……。",
+      "アニタ：じゃあ、スポーツ店に行きましょう。",
+      "ソック：買い物のあとで、ゲームコーナーに行きませんか。",
+      "アニタ：いいですね。",
+      "-----------------------------",
+      "ヒルニ：じゃ、行きましょうか。",
+      "ソック：あ、その前に、ちょっとトイレ。"
+    ],
+
+    translation: [
+      "1. (Đang đi bộ)",
+      "2. Anita: Sau đây chúng ta làm gì?",
+      "3. Hiruni: Tôi đói rồi. Đi ăn gì đó không?",
+      "4. Anita: Vậy thì đi đến khu food court nhé.",
+      "5. Sok: À, trước khi ăn, tôi muốn rút tiền...",
+      "6. Anita: Ở kia có ATM đó.",
+      "　",
+      " (Ở food court)",
+      "8. Hiruni: Sau đó tôi muốn mua giày jogging...",
+      "9. Anita: Vậy thì đi đến cửa hàng thể thao nhé.",
+      "10. Sok: Sau khi mua sắm xong, đi khu trò chơi game nhé?",
+      "11. Anita: Nghe hay đấy.",
+      "------------------------------",
+      "12. Hiruni: Vậy thì đi thôi nhé.",
+      "13. Sok: À, trước đó tôi đi toilet một chút."
+    ]
+  },
+  {
+    title: "Audio 13 : katachi",
+    audio: "audio/Y_[08-13]_katachi.mp3",
+    script: [
+      "食事の________、お金をおろし________。",
+      "このあと、ジョギングシューズを買い________。",
+      "買い物の________で、ゲームコーナーに行きませんか？"
+    ],
+
+    translation: [
+      "1. ________ bữa ăn, tôi muốn rút tiền. → 前に / たいんですが",
+      "2. Sau đó tôi muốn ________ giày jogging. → たいんですが",
+      "3. ________ khi mua sắm xong, đi khu trò chơi game nhé? → あと"
+    ]
+  },
+  {
+    title: "Audio 14 : hanasu1",
+    audio: "audio/Y_[08-14]_hanasu1.mp3",
+    script: [
+      "A：このあと、どうしますか。",
+      "B：何か食べませんか。フードコートに行きましょう。",
+      "A：食事【しょくじ】の前に、お金をおろしたいんですが……。",
+      "B：あそこにATMがありますよ。"
+    ],
+
+    translation: [
+      "1. Sau đây chúng ta làm gì?",
+      "2. Đi ăn gì đó không? Đi đến khu food court nhé.",
+      "3. Trước khi ăn, tôi muốn rút tiền...",
+      "4. Ở kia có ATM đó."
+    ]
+  },
+  {
+    title: "Audio 15 : hanasu2",
+    audio: "audio/Y_[08-15]_hanasu2.mp3",
+    script: [
+      "A：このあと、どうしますか。",
+      "B：フードコートに行きましょう。",
+      "A：いいですね。食事【しょくじ】のあとで、ゲームコーナーに行きませんか。",
+      "B：いいですね。"
+    ],
+
+    translation: [
+      "1. Sau đây chúng ta làm gì?",
+      "2. Đi đến khu food court nhé.",
+      "3. Nghe hay đấy. Sau khi ăn xong, đi khu trò chơi game nhé?",
+      "4. Hay đấy."
+    ]
+  },
+  {
+    title: "Audio 16 : kiku1",
+    audio: "audio/Y_[08-16]_kiku1.mp3",
+    script: [
+      "A：動物園【どうぶつえん】、どうでしたか。",
+      "B：楽しかったです。パンダがかわいかったです。",
+      "A：私はサルがよかったです。",
+      "B：ああ、サルもかわいかったですね。また行きたいです。"
+    ],
+
+    translation: [
+      "1. Sở thú thế nào?",
+      "2. Rất vui. Gấu trúc dễ thương lắm.",
+      "3. Tôi thì thích khỉ.",
+      "4. À, khỉ cũng dễ thương nhỉ. Tôi muốn đi lại lần nữa."
+    ]
+  },
+  {
+    title: "Audio 17 : kiku2",
+    audio: "audio/Y_[08-17]_kiku2.mp3",
+    script: [
+      "A：今日の展覧会【てんらんかい】、どうでしたか。",
+      "B：うーん。ちょっと難しかったです。",
+      "A：あー、現代【げんだい】の作品【さくひん】はねえ。",
+      "B：よくわかりませんでした。"
+    ],
+
+    translation: [
+      "1. Triển lãm hôm nay thế nào?",
+      "2. Ừm... hơi khó hiểu.",
+      "3. À... tác phẩm nghệ thuật hiện đại mà.",
+      "4. Tôi không hiểu lắm."
+    ]
+  },
+  {
+    title: "Audio 18 : kiku3",
+    audio: "audio/Y_[08-18]_kiku3.mp3",
+    script: [
+      "A：プロレス、どうでしたか。",
+      "B：はじめてでしたけど、とてもおもしろかったです。",
+      "A：そうですか。それはよかったです。",
+      "B：ほんと、すごかったです。また見たいです。"
+    ],
+
+    translation: [
+      "1. Đấu vật thế nào?",
+      "2. Tuy là lần đầu nhưng rất thú vị.",
+      "3. Vậy à. Thế thì tốt quá.",
+      "4. Thật sự rất tuyệt. Tôi muốn xem lại lần nữa."
+    ]
+  },
+  {
+    title: "Audio 19 : kiku4",
+    audio: "audio/Y_[08-19]_kiku4.mp3",
+    script: [
+      "A：映画、よかったですね。",
+      "B：感動【かんどう】しました。本当【ほんとう】にすばらしかったです。",
+      "A：じゃあ、また行きましょう。",
+      "B：今度【こんど】は、ホラー映画を見に行きましょう。",
+      "A：えー。"
+    ],
+
+    translation: [
+      "1. Bộ phim hay nhỉ.",
+      "2. Tôi rất cảm động. Thật sự tuyệt vời.",
+      "3. Vậy thì lần sau đi nữa nhé.",
+      "4. Lần tới đi xem phim kinh dị nhé.",
+      "5. Ơ…"
+    ]
+  },
+  {
+    title: "Audio 20 : katachi",
+    audio: "audio/Y_[08-20]_katachi.mp3",
+    script: [
+      "パンダが________です。",
+      "（展覧会【てんらんかい】は）ちょっと________です。",
+      "（プロレスは）はじめてでしたけど、とても________です。",
+      "ほんと、________です。",
+      "（映画は）本当【ほんとう】に________です。"
+    ],
+
+    translation: [
+      "1. Gấu trúc ________. → かわいかった",
+      "2. (Triển lãm) hơi ________. → 難しかった",
+      "3. (Đấu vật) tuy là lần đầu nhưng rất ________. → おもしろかった",
+      "4. Thật sự ________. → すごかった",
+      "5. (Bộ phim) thật sự ________. → すばらしかった"
+    ]
+  },
+  {
+    title: "Audio 21 : hanasu1",
+    audio: "audio/Y_[08-21]_hanasu1.mp3",
+    script: [
+      "A：動物園【どうぶつえん】、どうでしたか。",
+      "B：楽しかったです。パンダがかわいかったです。",
+      "A：そうですか。",
+      "B：また行きたいです。"
+    ],
+
+    translation: [
+      "1. Sở thú thế nào?",
+      "2. Rất vui. Gấu trúc dễ thương.",
+      "3. Vậy à.",
+      "4. Tôi muốn đi lại lần nữa."
+    ]
+  },
+  {
+    title: "Audio 22 : hanasu2",
+    audio: "audio/Y_[08-22]_hanasu2.mp3",
+    script: [
+      "A：展覧会【てんらんかい】、どうでしたか。",
+      "B：ちょっと難しかったです。",
+      "A：そうですか。",
+      "B：よくわかりませんでした。"
+    ],
+
+    translation: [
+      "1. triển lãm thế nào?",
+      "2. Hơi khó.",
+      "3. Vậy à.",
+      "4. Tôi không hiểu lắm."
+    ]
+  },
 ];
